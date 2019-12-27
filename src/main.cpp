cf0a132e3e879a04e337b02a5f4077fa7407dc51
zhuangjianjia <670344600@qq.com>
2019-12-27 14:36:03 +0800

    init

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <duktape.h>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    cout << "DUK_DEBUG_PROTOCOL_VERSION = " << DUK_DEBUG_PROTOCOL_VERSION << endl;
+    cout << "hello tux" << endl;
+    return 0;
+}
